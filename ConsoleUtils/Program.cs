c00cabb98d8f92743df4c1956ff55de1ce4e8090
Nketiah <gyatabajoe@gmail.com>
2025-02-04 09:56:44 +0000

    Add project files.

---
+++ ConsoleUtils/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleUtils.Utils;
+
+namespace ConsoleUtils
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Lottery lottery = new Lottery();
+            var draws = lottery.GetMeDrawNumbers(5, 0, 9, false);
+            Console.WriteLine("Draw Numbers (0-9): {0}", string.Join(", ", draws));
+
+        }
+    }
+}
---
+++ ConsoleUtils/Utils/Lottery.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleUtils.Utils
+{
+    public class Lottery
+    {
+        private static Random random = new Random();
+
+        // Method for generating draw numbers based on uniqueness flag
+        public List<int> GetMeDrawNumbers(int count, int rangeStart, int rangeEnd, bool isUnique)
+        {
+            List<int> numbers = new List<int>();
+
+            if (isUnique)
+            {
+                // Generate unique numbers using a HashSet to ensure no duplicates
+                HashSet<int> uniqueNumbers = new HashSet<int>();
+
+                while (uniqueNumbers.Count < count)
+                {
+                    int number = random.Next(rangeStart, rangeEnd + 1);
+                    uniqueNumbers.Add(number);
+                }
+
+                numbers.AddRange(uniqueNumbers);  // Convert HashSet to List
+            }
+            else
+            {
+                // Generate numbers with duplicates allowed
+                for (int i = 0; i < count; i++)
+                {
+                    int number = random.Next(rangeStart, rangeEnd + 1);
+                    numbers.Add(number);
+                }
+            }
+
+            return numbers;
+        }
+    }
+}
